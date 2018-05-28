b79e2d47bc56644ae3fece8aad1fa748c7c6958d
Stefan Slaveykov <wizard.oneandonly@gmail.com>
2018-05-28 23:15:22 +1200

    fix test, wow

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from '../components/App';
+import { BrowserRouter } from 'react-router-dom';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<BrowserRouter><App/></BrowserRouter>, div);
 });
