a1ad29f2800eaf30841bc40d60cdc0c5e8c08fae
Stefan Slaveykov <wizard.oneandonly@gmail.com>
2018-06-06 13:57:51 +1200

    add tests for private route HOC

--- src/test/PrivateRoute.test.tsx
+++ src/test/PrivateRoute.test.tsx
@@ -1,9 +1,27 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import * as enzyme from 'enzyme';
+import { shallow } from 'enzyme';
+import { BrowserRouter, Redirect } from 'react-router-dom';
+import Auth from '../Auth';
 import PrivateRoute from '../components/PrivateRoute';
-import { BrowserRouter } from 'react-router-dom';
 
-it('renders without crashing', () => {
-  const privateRoute = enzyme.shallow(<PrivateRoute>);
+describe('PrivateRoute', () => {
+  let Dummy, privateRoute;
+
+  beforeEach(() => {
+    Dummy = () => <div>Dummy component</div>;
+    privateRoute = shallow(<BrowserRouter><PrivateRoute component={Dummy} path='/dummy'/></BrowserRouter>);
+  });
+
+  it('passes the authentication check and renders the component', () => {
+    Auth.getToken = jest.fn(() => 'aaaa');
+
+    expect(privateRoute.html()).toEqual('<div>Dummy component</div>');
+  });
+
+  it('does not pass the authentication check and redirects', () => {
+    Auth.getToken = jest.fn(() => null);
+
+    expect(privateRoute.html()).toEqual('');
+  });
 });
