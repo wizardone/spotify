1e349b6c3a7e444c36955812588dfed5a6df56ee
Stefan Slaveykov <wizard.oneandonly@gmail.com>
2018-06-08 11:24:29 +1200

    add more basic tests for App

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -1,9 +1,26 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from '../components/App';
-import { BrowserRouter } from 'react-router-dom';
+import Auth from '../auth';
+import { BrowserRouter, Link } from 'react-router-dom';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<BrowserRouter><App/></BrowserRouter>, div);
+describe('App', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<BrowserRouter><App/></BrowserRouter>, div);
+  });
+
+  it('renders the proper header links if you are not logged in', () => {
+    const app = shallow(<App />);
+
+    expect(app.find(Link)).toHaveLength(2);
+  });
+
+  it('renders the proper header links if you are logged in', () => {
+    Auth.getToken = jest.fn(() => 'token');
+    const app = shallow(<App />);
+
+    expect(app.find(Link)).toHaveLength(3);
+  });
 });
